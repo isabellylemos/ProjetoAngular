6fd108d9eadd29713554e77a935d4c05fc62f2dd
Isabelly Lemos <isabellylemos.ml@gmail.com>
2022-06-09 11:04:47 -0300

    consumo de API - Login e cadastro

--- blogpessoalisabelly/src/app/entrar/entrar.component.ts
+++ blogpessoalisabelly/src/app/entrar/entrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -6,10 +10,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entrar.component.css']
 })
 export class EntrarComponent implements OnInit {
+usuarioLogin : UsuarioLogin = new UsuarioLogin()
 
-  constructor() { }
+  constructor(
+    private authService : AuthService,
+    private router : Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0) 
   }
 
+  entrar() {
+    this.authService.entrar(this.usuarioLogin).subscribe({
+        next: (resp: UsuarioLogin)=>{
+        this.usuarioLogin = resp
+        environment.token = this.usuarioLogin.token
+        environment.nome = this.usuarioLogin.nome
+        environment.foto = this.usuarioLogin.foto
+        environment.id = this.usuarioLogin.id
+        
+        this.router.navigate(["/inicio"])
+      },
+      error: erro => {
+        if(erro.status == 401) {
+          alert('Atenção! Usuario ou senha incorretos.')
+    
+        }
+      },
+    });
+  }
 }
+
+// entrar(){
+ // this.auth.entrar(this.usuarioLogin).subscribe((resp: UsuarioLogin)=>{
+  //  this.usuarioLogin = resp
+//environment.token = this.usuarioLogin.token
+  //  environment.nome = this.usuarioLogin.nome
+    //environment.foto = this.usuarioLogin.foto
+    //environment.id = this.usuarioLogin.id
+
+
+
+   // this.router.navigate(['/inicio'])
+  //}, erro =>{
+    //if(erro.status == 500){
+     // alert('Usuario ou senha estão incorretos!')
+   // }
+  //})
+
+//}
+
+//}
